5c0738254ac1459fbeccd370cea806ca09df5254
Jean Ribes <jean.christophe.ribes@sfr.fr>
2019-05-05 12:28:56 +0200

    échantillons annotés pour du machine learning

--- audio_fft.py
+++ audio_fft.py
@@ -2,12 +2,20 @@ from scipy.io.wavfile import *
 from scipy.fftpack import fft
 import numpy as np
 from matplotlib import pyplot as plt
-N=128
+import json
+N=128*2
 T=1.0/10000.0
-for i in range(1, 6): #boucle pour traiter 6 fichiers
+data: dict = {"N":N, "fichiers":[]}
+for i in range(1, 9): #boucle pour traiter 6 fichiers
     print(i)
     fe, audio = read('bonjour p2i/{}.wav'.format(i))#on lit chaque fichier audio
-    coefs = np.abs(fft(audio, N)[N//2:N]) #partie réelle positive
+    coefs = np.abs(fft(audio, N)[0:N//2]) #partie réelle positive #[N//2:N]
+    data['fichiers'].append(
+        {
+            "nom": "{}.wav".format(i),
+            "coefs":coefs.tolist()
+        }
+    )
     x, y = [], []
     j = 0
     for v in coefs: #présentation jolie du graph fourier
@@ -22,12 +30,18 @@ for i in range(1, 6): #boucle pour traiter 6 fichiers
     plt.ylabel('amplitude')
     plt.xlabel('fréquence')
     plt.show()
-    with open('coefs test jean.txt', 'a+') as f:
-        f.write('audio: bonjour p2i jean {}'.format(i))
-        f.write('\n[')
-        for c in coefs:
-            f.write(str(c))
-            f.write(' , ')
-        f.write(']\n----------------')
-        f.write('\n')
-    f.close()
+    n: int = 0
+    #with open('coefs test jean.txt', 'a+') as f:
+    #    f.write('audio: bonjour p2i jean {}'.format(i))
+    #    f.write('\n[')
+    #    for c in coefs:
+    #        f.write(str(c))
+    #        f.write(' , ')
+    #        n+=1
+    #        print(n)
+    #    f.write(']\n----------------')
+    #    f.write('\n')
+    #f.close()
+with open('data2.json', 'w+') as f:
+    json.dump(data, f, sort_keys=True, indent=4)
+f.close()
