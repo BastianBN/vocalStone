1d5ee5d248d4a0cdf98c1464ea2e626b3a11c743
Jean Ribes <jean.christophe.ribes@sfr.fr>
2019-05-05 12:57:10 +0200

    decision tree qui marche !

---
+++ classification.py
@@ -0,0 +1,21 @@
+import json
+from sklearn import tree
+
+data_learn, data_test={},{}
+with open('data_learn.json', 'r') as f:
+    data_learn = json.load(f)
+f.close()
+with open('data_test.json', 'r') as f:
+    data_test = json.load(f)
+f.close()
+#voix = [x['coefs'] for x in data['fichiers'] if x['type'] == "voix"]
+#sinus = [x['coefs'] for x in data['fichiers'] if x['type'] == "sinus"]
+
+X = [x['coefs'] for x in data_learn['fichiers']]
+Y = [x['type']  for x in data_learn['fichiers']]
+
+modele = tree.DecisionTreeClassifier()
+modele.fit(X, Y)
+
+X_test = [x['coefs'] for x in data_test['fichiers']]
+Y_test = [x['type']  for x in data_test['fichiers']]
